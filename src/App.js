561528323c392016017afe6f1605cfca94e97d3e
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-23 23:12:34 -0300

    fornecendo provider ao App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+      <Table />
+    </PlanetsProvider>
   );
 }
-// abrindo pr
+
 export default App;
