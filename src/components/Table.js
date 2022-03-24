ff56e9a25f24d25bea4d670e877dd3b2134a8199
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-23 23:08:01 -0300

    criando header da tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,15 @@
+import React, { useContext, useEffect } from 'react';
 function Table() {
+  const headerList = [
+    'Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate', 'Gravity',
+    'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited', 'URL',
+  ];
 
   return (
     <table>
       <thead>
         <tr>
+          { headerList.map((header) => <th key={ header }>{ header }</th>)}
         </tr>
       </thead>
       <tbody>
