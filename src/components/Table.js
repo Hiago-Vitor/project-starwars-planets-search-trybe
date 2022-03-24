a30a8e5441822e822ca674f7829b3b17d2ca26e7
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-23 23:06:02 -0300

    criando componente funcional generico

---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+function Table() {
+
+  return (
+    <table>
+      <thead>
+        <tr>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
