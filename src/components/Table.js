c98b67f7d16441ca5694dda828c932ccb4a45766
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-23 23:14:08 -0300

    preenchendo tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,19 @@
 import React, { useContext, useEffect } from 'react';
+import planetsContext from '../context/planetsContext';
+import fetchPlanetList from '../services/dataAPI';
+
 function Table() {
+  const { data, setPlanetsData } = useContext(planetsContext);
+
+  useEffect(() => {
+    async function getPlanetsData() {
+      const listPlanets = await fetchPlanetList();
+      listPlanets.map((planet) => delete planet.residents);
+      setPlanetsData(listPlanets);
+    }
+    getPlanetsData();
+  }, [setPlanetsData]);
+
   const headerList = [
     'Name', 'Rotation Period', 'Orbital Period', 'Diameter', 'Climate', 'Gravity',
     'Terrain', 'Surface Water', 'Population', 'Films', 'Created', 'Edited', 'URL',
@@ -13,6 +27,27 @@ function Table() {
         </tr>
       </thead>
       <tbody>
+        { data.map(({
+          name, rotation_period: rtPeriod, orbital_period: orbtPeriod, diameter, climate,
+          gravity, terrain, surface_water: srfcWater, population, films, created, edited,
+          url,
+        }) => (
+          <tr key={ name }>
+            <td>{ name }</td>
+            <td>{ rtPeriod }</td>
+            <td>{ orbtPeriod }</td>
+            <td>{ diameter }</td>
+            <td>{ climate }</td>
+            <td>{ gravity }</td>
+            <td>{ terrain }</td>
+            <td>{ srfcWater }</td>
+            <td>{ population }</td>
+            <td>{ films }</td>
+            <td>{ created }</td>
+            <td>{ edited }</td>
+            <td>{ url }</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
