4a84f68604d07c84d261188282bc22d2df861d3b
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-24 18:44:10 -0300

    criando fomr e input para o filtro

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,19 @@
+import React, { useContext } from 'react';
+
 function Search() {
 
   return (
+    <form>
+      <label htmlFor="user">
+        Projeto Star Wars - Trybe
+        <input
+          type="text"
+          name="name"
+          data-testid="name-filter"
+          placeholder="Escreva o nome de um planeta"
+        />
+      </label>
+    </form>
   );
 }
 
