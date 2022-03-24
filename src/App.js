c90a57b9bdf1e6625feccc9d589612d5f9a2c407
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-24 18:42:12 -0300

    add componente ao App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Search from './components/Search';
 import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
   return (
     <PlanetsProvider>
+      <Search />
       <Table />
     </PlanetsProvider>
   );
