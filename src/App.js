d0e56a8bc5d15432b1a9d6a85f6f006e90d8a5a8
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-28 13:23:11 -0300

    adicionando componente ao App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Currentfilters from './components/CurrentFilters';
 import Search from './components/Search';
 import Table from './components/Table';
 import PlanetsProvider from './context/PlanetsProvider';
@@ -8,6 +9,7 @@ function App() {
   return (
     <PlanetsProvider>
       <Search />
+      <Currentfilters />
       <Table />
     </PlanetsProvider>
   );
