e44988f848078df9b7784f16ec885d6e157ba1f8
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-28 13:21:42 -0300

    criando componente para exibir filtros ativos

---
+++ src/components/CurrentFilters.js
@@ -0,0 +1,18 @@
+function Currentfilters() {
+
+  return (
+    <section>
+          <p
+            data-testid="filter"
+          >
+            <button
+              type="button"
+            >
+              ❌
+            </button>
+          </p>
+    </section>
+  );
+}
+
+export default Currentfilters;
