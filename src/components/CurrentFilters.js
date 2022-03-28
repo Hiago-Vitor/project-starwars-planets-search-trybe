b3584464f6461b09492fdc3b86ce9e282d7538f2
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-28 13:22:15 -0300

    adicionando leitura e exibição dos filtros

--- src/components/CurrentFilters.js
+++ src/components/CurrentFilters.js
@@ -1,16 +1,38 @@
+import React, { useContext } from 'react';
+import planetsContext from '../context/planetsContext';
+
 function Currentfilters() {
+  const { filterByNumericValues, setFilterByNumericValues } = useContext(planetsContext);
+
+  const removeFilter = (currentValue) => {
+    setFilterByNumericValues(
+      filterByNumericValues.filter((filter) => filter !== currentValue),
+    );
+  };
 
   return (
     <section>
+      {
+        filterByNumericValues.map((currentValue) => (
           <p
             data-testid="filter"
+            key={ currentValue.column }
           >
+            { currentValue.column }
+            {' '}
+            { currentValue.comparison }
+            {' '}
+            { currentValue.value }
+            {' '}
             <button
               type="button"
+              onClick={ () => removeFilter(currentValue) }
             >
               ❌
             </button>
           </p>
+        ))
+      }
     </section>
   );
 }
