08d8dc626a06f296031dc51b3fa7dedcf7408191
Hiago-Vitor <silvahiago1599@gmail.com>
2022-03-27 22:09:28 -0300

    criando os novos inputs

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,13 @@ import React, { useContext } from 'react';
 import planetsContext from '../context/planetsContext';
 
 function Search() {
-  const { setFilterByName } = useContext(planetsContext);
+
+  const comparisons = ['maior que', 'menor que', 'igual a'];
+
+  const columns = [
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ];
+
   const handleChanges = (target) => {
     const newFilter = { name: target.value.toLowerCase() };
     setFilterByName(newFilter);
@@ -19,6 +25,32 @@ function Search() {
           onChange={ ({ target }) => handleChanges(target) }
         />
       </label>
+      <label htmlFor="column">
+        Coluna
+        <select
+          data-testid="column-filter"
+          name="column"
+        >
+          { columns.map((column) => (<option key={ column }>{ column }</option>))}
+        </select>
+      </label>
+      <label htmlFor="comparison">
+        Operador
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+        >
+          { comparisons.map(
+            (comparison) => (<option key={ comparison }>{ comparison }</option>),
+          )}
+        </select>
+      </label>
+      <input
+        data-testid="value-filter"
+        type="number"
+        name="value"
+      />
+      <button type="submit" data-testid="button-filter">Filtrar</button>
     </form>
   );
 }
